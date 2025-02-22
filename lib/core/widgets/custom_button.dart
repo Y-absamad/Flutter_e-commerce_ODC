5dab83457f1de7cbae307fc55eaecce2947e7cf6
Yousef Abdel Samad <yousef.ahmed.0402@gmail.com>
2025-02-22 16:51:03 +0200

    login and register

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../constants/app_color.dart';
+
+class CustomButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      decoration: BoxDecoration(
+        color: AppColor.buttonColor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: CupertinoButton(
+        onPressed: onPressed,
+        child: Text(
+          label,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/feature/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+ bool isLogin = false;
+  String? token;
+
+@override
+  void initState() {
+    //CacheHelper.init();
+    //token = CacheHelper.getData(key: "token");
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: FlutterSplashScreen.fadeIn(
+          //nextScreen: token != null ? HomeScreen() : LoginScreen(),
+          //nextScreen: ,
+          backgroundColor: Colors.white,
+          duration: const Duration(milliseconds: 3515),
+          onInit: () async {
+            
+          },
+          onEnd: () async {
+            debugPrint("onEnd 1");
+          },
+          childWidget: SizedBox(
+            height: 200,
+            width: 200,
+            child: Image.network(
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1280px-Amazon_logo.svg.png"),
+          ),
+        ),
+      ),
+    );
+
+  }
+}
