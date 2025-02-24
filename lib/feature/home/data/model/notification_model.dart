4fc5dd2e2183f2006c79728e54100eb299318861
Yousef Abdel Samad <yousef.ahmed.0402@gmail.com>
2025-02-24 17:26:15 +0200

    add notification

---
+++ lib/feature/home/data/model/notification_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotificationModel {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+
+  NotificationModel({
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+  });
+}
