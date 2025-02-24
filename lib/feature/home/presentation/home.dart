a259b381359091a5e172ff013015c5bd331fdfce
Yousef Abdel Samad <yousef.ahmed.0402@gmail.com>
2025-02-24 21:05:41 +0200

    add browse to home

---
+++ lib/feature/home/presentation/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_odc/feature/home/presentation/widgets/browse_screen.dart';
+import 'package:flutter_ecommerce_odc/feature/home/presentation/screens/home_screen.dart';
+
+import '../../profile/presentation/screens/profile_screen.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    HomeScreen(),
+    BrowseScreen(),
+    //WishlistPage(),
+    //CartPage(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Browse"),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: "Wishlist"),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
